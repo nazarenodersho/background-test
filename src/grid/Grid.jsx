afc8e8628905a192cb2775a4660dee5bcad42cfd
NazarenoDersho <nazarenodersho@gmail.com>
2024-09-20 11:30:53 -0300

    UseEffect updated for better perfomance

--- src/grid/Grid.jsx
+++ src/grid/Grid.jsx
@@ -7,57 +7,75 @@ export function Grid() {
     <div key={i} className={styles.square}></div>
   ));
 
+  // useEffect(() => {
+  //   const updateCursor = ({ clientX, clientY }) => {
+  //     document.documentElement.style.setProperty("--x", clientX);
+  //     document.documentElement.style.setProperty("--y", clientY);
+  //   };
+
+  //   const activateEffect = () => {
+  //     document.documentElement.style.setProperty("--active", 0);
+  //   };
+
+  //   const deactivateEffect = () => {
+  //     document.documentElement.style.setProperty("--active", 0);
+  //   };
+
+  //   document.body.addEventListener("pointermove", updateCursor);
+  //   document.body.addEventListener("pointerover", activateEffect);
+  //   document.body.addEventListener("pointerout", deactivateEffect);
+
+  //   const handleTouchStart = (e) => {
+  //     const touch = e.touches[0];
+  //     if (touch) {
+  //       updateCursor(touch);
+  //       activateEffect();
+  //     }
+  //   };
+
+  //   const handleTouchMove = (e) => {
+  //     const touch = e.touches[0];
+  //     if (touch) {
+  //       updateCursor(touch);
+  //     }
+  //   };
+
+  //   const handleTouchEnd = () => {
+  //     deactivateEffect();
+  //   };
+
+  //   document.body.addEventListener("touchstart", handleTouchStart);
+  //   document.body.addEventListener("touchmove", handleTouchMove);
+  //   document.body.addEventListener("touchend", handleTouchEnd);
+  //   document.body.addEventListener("touchcancel", handleTouchEnd);
+
+  //   return () => {
+  //     document.body.removeEventListener("pointermove", updateCursor);
+  //     document.body.removeEventListener("pointerover", activateEffect);
+  //     document.body.removeEventListener("pointerout", deactivateEffect);
+
+  //     document.body.removeEventListener("touchstart", handleTouchStart);
+  //     document.body.removeEventListener("touchmove", handleTouchMove);
+  //     document.body.removeEventListener("touchend", handleTouchEnd);
+  //     document.body.removeEventListener("touchcancel", handleTouchEnd);
+  //   };
+  // }, []);
+
   useEffect(() => {
-    const updateCursor = ({ clientX, clientY }) => {
-      document.documentElement.style.setProperty("--x", clientX);
-      document.documentElement.style.setProperty("--y", clientY);
+    const updateCursor = (event) => {
+      const x = event.touches ? event.touches[0].clientX : event.clientX;
+      const y = event.touches ? event.touches[0].clientY : event.clientY;
+
+      document.documentElement.style.setProperty("--x", x);
+      document.documentElement.style.setProperty("--y", y);
     };
 
-    const activateEffect = () => {
-      document.documentElement.style.setProperty("--active", 0);
-    };
-
-    const deactivateEffect = () => {
-      document.documentElement.style.setProperty("--active", 0);
-    };
-
-    document.body.addEventListener("pointermove", updateCursor);
-    document.body.addEventListener("pointerover", activateEffect);
-    document.body.addEventListener("pointerout", deactivateEffect);
-
-    const handleTouchStart = (e) => {
-      const touch = e.touches[0];
-      if (touch) {
-        updateCursor(touch);
-        activateEffect();
-      }
-    };
-
-    const handleTouchMove = (e) => {
-      const touch = e.touches[0];
-      if (touch) {
-        updateCursor(touch);
-      }
-    };
-
-    const handleTouchEnd = () => {
-      deactivateEffect();
-    };
-
-    document.body.addEventListener("touchstart", handleTouchStart);
-    document.body.addEventListener("touchmove", handleTouchMove);
-    document.body.addEventListener("touchend", handleTouchEnd);
-    document.body.addEventListener("touchcancel", handleTouchEnd);
+    document.addEventListener("pointermove", updateCursor);
+    document.addEventListener("touchmove", updateCursor);
 
     return () => {
-      document.body.removeEventListener("pointermove", updateCursor);
-      document.body.removeEventListener("pointerover", activateEffect);
-      document.body.removeEventListener("pointerout", deactivateEffect);
-
-      document.body.removeEventListener("touchstart", handleTouchStart);
-      document.body.removeEventListener("touchmove", handleTouchMove);
-      document.body.removeEventListener("touchend", handleTouchEnd);
-      document.body.removeEventListener("touchcancel", handleTouchEnd);
+      document.removeEventListener("pointermove", updateCursor);
+      document.removeEventListener("touchmove", updateCursor);
     };
   }, []);
 
